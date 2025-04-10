a53d988f8a9b359d1799fc4f7e27c7be76486197
Quentin Munch <neurotronic67@gmail.com>
2025-04-10 22:16:24 +0200

    some change to gamepad

--- deployment/CTRL/Gamepad.py
+++ deployment/CTRL/Gamepad.py
@@ -13,6 +13,7 @@ class Gamepad:
         self.deadzone = deadzone
 
         self.last_commands = [0.0, 0.0, 0.0]
+        self.last_head_tilt = 0.0
         self.last_left_trigger = 0.0
         self.last_right_trigger = 0.0
 
@@ -30,10 +31,10 @@ class Gamepad:
             time.sleep(1 / self.command_freq)
 
     def get_commands(self):
-        A_pressed = False
+        C_pressed = False
         X_pressed = False
-        B_pressed = False
-        Y_pressed = False
+        S_pressed = False
+        T_pressed = False
 
         last_commands = self.last_commands
 
@@ -82,36 +83,36 @@ class Gamepad:
         last_commands[2] = ang_vel
 
         for event in pygame.event.get():
-            if self.p1.get_button(0):  # A button
-                A_pressed = True
-
-            if self.p1.get_button(3):  # X button
+            if self.p1.get_button(0):  # X button
                 X_pressed = True
 
-            if self.p1.get_button(1):  # B button
-                B_pressed = True
+            if self.p1.get_button(3):  # square button
+                S_pressed = True
 
-            if self.p1.get_button(2):  # Y button
-                Y_pressed = True
+            if self.p1.get_button(1):  # circle button
+                C_pressed = True
+
+            if self.p1.get_button(2):  # triangle button
+                T_pressed = True
 
         pygame.event.pump()  # process event queue
 
-        return np.around(last_commands, 3), head_t, A_pressed, X_pressed, B_pressed, Y_pressed
+        return np.around(last_commands, 3), head_t, S_pressed, T_pressed, C_pressed, X_pressed
 
     def get_last_command(self):
-        A_pressed = False
+        C_pressed = False
         X_pressed = False
-        B_pressed = False
-        Y_pressed = False
+        S_pressed = False
+        T_pressed = False
         try:
-            self.last_commands, head_tilt, A_pressed, X_pressed, B_pressed, Y_pressed = self.cmd_queue.get(False)  # non blocking
+            self.last_commands, self.last_head_tilt, S_pressed, T_pressed, C_pressed, X_pressed = self.cmd_queue.get(False)  # non blocking
         except Exception:
             pass
-        return self.last_commands, head_tilt, A_pressed, X_pressed, B_pressed, Y_pressed
+        return self.last_commands, self.last_head_tilt, S_pressed, T_pressed, C_pressed, X_pressed
 
 
 if __name__ == "__main__":
-    controller = Gamepad(command_freq=20, vel_range_x=[-0.6, 0.6], vel_range_y=[-0.6, 0.6], vel_range_rot=[-1.0, 1.0], deadzone=0.03)
+    controller = Gamepad(command_freq=20, vel_range_x=[-0.6, 0.6], vel_range_y=[-0.6, 0.6], vel_range_rot=[-1.0, 1.0], deadzone=0.05)
 
     while True:
         print(controller.get_last_command())
