cf3791b799d6f91e48b65498d2a97299fb527a1f
Quentin <neurotronic67@gmail.com>
2025-05-26 20:48:36 +0200

    position control + id servos

--- deployment/script/CTRL/Servo_Controller_BD5.py
+++ deployment/script/CTRL/Servo_Controller_BD5.py
@@ -413,6 +413,7 @@ if __name__=='__main__':
             break
 
     # Activate + Set default angles
+    BDX.set_PID(pid=[800, 0, 0])
     BDX.enable_torque()
     BDX.set_position(default_angles_full)
 
